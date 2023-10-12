5a9f940e5108ab22ffb9b6c7becd347f1aa85bf3
Filipinski <140195559+filipinski1@users.noreply.github.com>
2023-10-12 11:52:07 -0400

    Add project files.

---
+++ Enums/ReadyStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+namespace TheBlogProject.Enums
+{
+    public enum ReadyStatus
+    {
+        Incomplete,
+        ProductionReady,
+        PreviewReady
+    }
+}
